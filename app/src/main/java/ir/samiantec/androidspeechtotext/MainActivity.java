5a5917589fbf6a14a38fe49af96aae763830437a
Sina <Moradix96@gmail.com>
2022-02-12 00:11:08 +0330

    Implement functionality based on https://www.geeksforgeeks.org/how-to-convert-speech-to-text-in-android/ and https://stackoom.com/en/question/2UgTE

--- app/src/main/java/ir/samiantec/androidspeechtotext/MainActivity.java
+++ app/src/main/java/ir/samiantec/androidspeechtotext/MainActivity.java
@@ -1,14 +1,53 @@
 package ir.samiantec.androidspeechtotext;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1;
+    private Button btnMic;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMic = findViewById(R.id.btnMic);
+        tvResult = findViewById(R.id.tvResult);
+
+        btnMic.setOnClickListener(v -> {
+            final Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "fa");
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
+
+            try {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            if (resultCode == RESULT_OK && data != null) {
+                final ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                tvResult.setText(Objects.requireNonNull(result).get(0));
+            }
+        }
     }
 }
